c408546ea827ef64647276caed3d79ef74cb2e73
Pawel12314 <pkrzywosz97@gmail.com>
2022-10-16 17:34:38 +0200

    created project for q sharp with c sharp

---
+++ QsharpTopScoringPair/QsharpTopScoringPair/Driver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Simulators;
+//using static Microsoft.Quantum.QsCompiler.SyntaxTokens.QsExpressionKind<Expr, Symbol, Type>;
+
+
+using System;
+
+using Microsoft.Quantum.Simulation.Core;
+
+//using static Microsoft.Quantum.QsCompiler.SyntaxTokens.QsExpressionKind<Expr, Symbol, Type>;
+
+namespace Quantum.QsharpTopScoringPair
+{
+    public class Driver
+    {
+        static void Main(string[] args)
+        {
+            /* using (var sim = new QuantumSimulator())
+             {
+                 var res = HelloQ1.Run(sim, 20, 10).Result;
+                 Console.WriteLine(res);
+             }
+             Console.WriteLine("Press any key to continue...");
+             Console.ReadKey();*/
+            Console.Out.WriteLine("testing c sharp and q sharp");
+            var sim = new QuantumSimulator();
+            var res = HelloQ1.Run(sim).Result;
+            Console.WriteLine(res);
+            
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ QsharpTopScoringPair/QsharpTopScoringPair/Program.qs
@@ -0,0 +1,13 @@
+﻿
+namespace Quantum.QsharpTopScoringPair  
+{  
+    // Import Quantum.Primitive  
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;  
+      
+    // Create an operation that adds two int and returns a total int  
+   
+    operation HelloQ1 () : Unit {
+        Message("Hello quantum world!");
+    }
+} 
